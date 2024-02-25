1660b082f4502d6136d085f98f3f4112d77115e5
Sam <livaco@livaco.dev>
2024-02-25 17:32:42 +0000

    neovim complaining about formatting

--- Generics/Converter.py
+++ Generics/Converter.py
@@ -7,9 +7,9 @@ class Converter():
     # (Sam) Converter should be a anonymous function that converts a file from input -> output
     def addConversion(self, convertTo, converter):
         self.converters[convertTo] = converter
-    
+
     def convertFile(self, fileName, fileType):
         self.converters[fileType](fileName)
-    
+
     def readFile(self, fileName):
         return # (Sam) To be overridden by the converter.
